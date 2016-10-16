2fbdd44b880c430ff0fd2de0086701fe93aae1ed
Luis Machado Reis <luis.reis@aurea.com>
2016-10-16 17:01:41 -0200

    Dropping class constructor override - Java Developers from hell

--- tts-engine-agi/modules/tts-repository/JobFactory.php
+++ tts-engine-agi/modules/tts-repository/JobFactory.php
@@ -9,6 +9,14 @@ require_once("Job.php");
 class JobFactory {
 	static protected $instance = null;
 
+	static function getInstance($license, $key) {
+		if (is_null(static::$instance)) {
+			static::$instance = new JobFactory("https://api.ligflat.com.br/v2/tts", $license, $key);
+		}
+
+		return static::$instance;
+	}
+
 	static function getInstance($endpoint, $license, $key) {
 		if (is_null(static::$instance)) {
 			static::$instance = new JobFactory($endpoint, $license, $key);
@@ -20,11 +28,6 @@ class JobFactory {
 	private $license;
 	private $key;
 	private $endpoint = "https://api.ligflat.com.br/v2/tts";
-
-	final private function __construct($license, $key) {
-		$this->license = $license;
-		$this->key = $key;
-	}
 
 	final private function __construct($endpoint, $license, $key) {
 		$this->license = $license;
